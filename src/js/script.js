5d4b32c7ce5663efd145616adcbe74b3cf63d65b
Aleksey <ascetic.iziken@gmail.com>
2025-12-05 19:24:53 +0700

    add works section and swiper slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import "/src/sass/style.scss";
+
+try {
+  new Swiper(".works__slider", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: true,
+    navigation: {
+      nextEl: ".icon-right-open",
+      prevEl: ".icon-left-open",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
+    },
+    modules: [Navigation, Pagination],
+  });
+} catch (e) {}
