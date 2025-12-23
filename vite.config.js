b114f046bcb7c6d0ea3471567c66a089c47629ac
Aleksey <ascetic.iziken@gmail.com>
2025-12-23 09:52:38 +0700

    add catalog section

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
       },
     },
   },
-})
+});
